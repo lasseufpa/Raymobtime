f88686a28245bd44eac0cb11ece1943e4b9f6d41
Ailton Pinto de Oliveira <201707140062>
2020-06-10 13:34:12 -0300

    Add support for Rx Angle

--- Python_codes/InterpretRaymobtime.py
+++ Python_codes/InterpretRaymobtime.py
@@ -9,9 +9,10 @@ import channelRaysDiscardingInvalids as chin
 import os
 
 #Inputs
-numEpisodes = range(0,6,1) #interator that determines number of episodes to be read
-raymobtimepath = './insite_data_s004_carrier60GHz' #Insert the location of the files to be read
-filePrefix = 'rosslyn_mobile_60GHz_ts1s_V_e';           #Include the file name without the number that counts episodes
+numEpisodes = range(0,100,1) #interator that determines number of episodes to be read
+insite_version = 3.2 #consult on raymobtime datasets table
+raymobtimepath = './ray_tracing_data_s008_carrier60GHz' #Insert the location of the files to be read
+filePrefix = 'rosslyn_mobile_60GHz_ts0.1s_V_Lidar_e';           #Include the file name without the number that counts episodes
 extension ='.hdf5';        #Include file extension
 numOfInvalidChannels = 0 #For computing number of invalid channels, no need to change
 arrayName = 'allEpisodeData' #Array name, other arrays can be checked with "list(array.keys())" comand
@@ -44,8 +45,8 @@ for index in numEpisodes:
             [numParameters, numPaths] = channelRays.shape   #support files with 7 or 8 ray information (8 is ray phase)
             doesItIncludeRayPhase = 0
 
-            if (numParameters==8):
-                doesItIncludeRayPhase = 1
+            if (numParameters >= 8):
+                doesItIncludeRxAngle = 1
 
             gainMagnitude = channelRays[0,:] # received power in dBm
             timeOfArrival = channelRays[1,:] #time of arrival in seconds
@@ -54,14 +55,18 @@ for index in numEpisodes:
             AoA_el = channelRays[4,:] #elevation angle of arrival in degrees
             AoA_az = channelRays[5,:] #azimuth angle of arrival in degrees
             isLOS = channelRays[6,:]  #flag 1 for LOS ray, flag 0 for NLOS ray
-            if (doesItIncludeRayPhase == 0):
+            if (insite_version <= 3.2):
                 gainPhase = 2*math.pi*np.random.uniform(0,1,len(gainMagnitude))
                 # it was not specified in first 5gm data version, so if we don't
 				# have it, generate uniformly distributed phase
             else:
                 gainPhase = channelRays[7,:]*math.pi/180 #ray phases recorded from InSite
+            if doesItIncludeRxAngle:
+                RxAngle = channelRays[8,:]
 			
             gainMagnitude = np.power(10,(0.1*gainMagnitude)) #transform to linear
             complexGain=np.power(gainMagnitude,1j * gainPhase) #consider complex gain
             isChannelLOS = sum(isLOS)
+            #example: Return gainMagnitude (options - timeOfArrival, AoD_az, AoD_el, 
+            # AoA_az, AoA_el, isLOS, gainPhase, RxAngle)
             print(gainMagnitude)
